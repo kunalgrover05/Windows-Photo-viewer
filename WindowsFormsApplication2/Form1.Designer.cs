83aef383e79b71a0ec3df5be869799725ff325f0
unknown <kunalgrover05@gmail.com>
2014-03-17 06:32:28 +0530

    Initial commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Collections.ArrayList myarr;
+            
+        int flag = 0;
+        string folder_name;    
+        string[] filePaths = new string[100];
+        int i = 0;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.opendialog = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // opendialog
+            // 
+            this.opendialog.Location = new System.Drawing.Point(922, 557);
+            this.opendialog.Name = "opendialog";
+            this.opendialog.Size = new System.Drawing.Size(75, 23);
+            this.opendialog.TabIndex = 0;
+            this.opendialog.Text = "button1";
+            this.opendialog.UseVisualStyleBackColor = true;
+            this.opendialog.Click += new System.EventHandler(this.opendialog_Click);
+            // 
+            // folderBrowserDialog2
+            // 
+            this.folderBrowserDialog2.HelpRequest += new System.EventHandler(this.folderBrowserDialog2_HelpRequest);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(672, 567);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(997, 511);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 618);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.opendialog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button opendialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            myarr = new System.Collections.ArrayList();
+            InitializeComponent();
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.Thumbnail_Paint);
+            this.KeyPreview = true;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown_1);
+        }
+
+        private void KeyDown_1(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                if (i == myarr.Count - 1)
+                {
+                    i = 0;
+                }
+                else
+                {
+                    i = i + 1;
+                }
+                flag = 1;
+                pictureBox1.Invalidate();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                if (i == 0)
+                {
+                    i = myarr.Count - 1;
+                }
+                else
+                {
+                    i = i - 1;
+                }
+                flag = 1;
+                pictureBox1.Invalidate();
+            }
+            else if (e.KeyCode == Keys.Delete)
+            {
+                String temp = (String)myarr[i];
+                if (i == myarr.Count - 1)
+                {
+                    i = 0;
+                }
+                else
+                {
+                    i = i + 1;
+                }
+                flag = 1;
+                pictureBox1.Invalidate();
+                GC.Collect();
+
+                if (System.IO.File.Exists(temp))
+                {
+                    try
+                    {
+                        MessageBox.Show("Deleting " + temp);
+                        System.IO.File.Delete(temp);
+                        if (i == 0)
+                        {
+                            myarr.RemoveAt(myarr.Count - 1);
+                        }
+                        else
+                        {
+                            myarr.RemoveAt(i - 1);
+                            i = i - 1;
+                        }
+                    }
+                    catch (System.IO.IOException k)
+                    {
+                        MessageBox.Show(k.Message);
+                    }
+                }
+
+                if (System.IO.File.Exists(temp.Substring(0, temp.Length - 3) + "CR2"))
+                {
+                    try
+                    {
+                        //MessageBox.Show("deleting " + temp.Substring(0, temp.Length - 3) + "CR2");
+                        System.IO.File.Delete(temp.Substring(0, temp.Length - 3) + "CR2");
+                    }
+                    catch (System.IO.IOException k)
+                    {
+                        MessageBox.Show(k.Message);
+                    }
+                }
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+
+            }
+        }
+
+        private void opendialog_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                myarr.Clear();
+                foreach (String k in Directory.GetFiles(folderBrowserDialog2.SelectedPath, "*.JPG"))
+                {
+                    myarr.Add(k);
+                }
+
+                label1.Text = folder_name;
+                flag = 1;
+                pictureBox1.Invalidate();
+
+                //   pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+                //    pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.Thumbnail_Paint);
+            }
+        }
+
+        private void Thumbnail_Paint(object sender, PaintEventArgs e)
+        {
+            //GetThumbnail(e);
+            ShowImage(e);
+        }
+
+        void ShowImage(PaintEventArgs e)
+        {
+                try
+                {
+                    pictureBox1.Image = new Bitmap((String)myarr[i]);
+                 
+               // Image.GetThumbnailImageAbort callback =
+               //       new Image.GetThumbnailImageAbort(ThumbnailCallback);
+               /* Image image = new Bitmap((String)myarr[i]);
+                Image thumb_im = image.GetThumbnailImage(600,400, callback, new
+                           IntPtr());
+                    e.Graphics.DrawImage(
+                               thumb_im,
+                               0,
+                               0);
+                    image = null;
+                    GC.Collect();
+               **/
+                }
+                catch
+                {
+                }
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog2_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        public bool ThumbnailCallback()
+        {
+            return true;
+        }
+
+        private void GetThumbnail(PaintEventArgs e)
+        {
+            Image.GetThumbnailImageAbort callback =
+                new Image.GetThumbnailImageAbort(ThumbnailCallback);
+            if (flag == 1)
+            {
+                for (int j = 0; j < 4; ++j)
+                {
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        Image image = new Bitmap(filePaths[j * 4 + i]);
+                        Image pThumbnail = image.GetThumbnailImage(200, 150, callback, new
+                           IntPtr());
+                        //label1.Text = filePaths[j*2 +i];
+                        e.Graphics.DrawImage(
+                           pThumbnail,
+                           i * 230 + 20,
+                           j * 160 + 10,
+                           pThumbnail.Width,
+                           pThumbnail.Height);
+                        image = null;
+                        pThumbnail = null;
+                        GC.Collect();
+                    }
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+    }
+}
